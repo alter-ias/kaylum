15d8365e9b476e6030110e92a2ec161c26a3e2e2
alterlab <agencia.alter.ias@gmail.com>
2025-07-11 00:23:04 -0600

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,43 @@
+const CACHE_NAME = 'kaylum-cache-v2'; // Cambia la versión si haces cambios para forzar la actualización
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/player.html',
+  '/style.css',
+  '/app.js',
+  'https://cdn.jsdelivr.net/npm/fuse.js@7.0.0'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Cache abierto');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
+});
+
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
